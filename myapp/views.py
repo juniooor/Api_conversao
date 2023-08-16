072d6b7983df2e810b7445d15dc7c2925f8581e1
juniooor <juniormodern@hotmail.com>
2023-08-15 22:07:43 -0300

    teste

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from myapp.serializers import UserSerializer, GroupSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_class = [permissions.IsAuthenticated]
+    
+    
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_class = [permissions.IsAuthenticated]
+    
 
 # Create your views here.
